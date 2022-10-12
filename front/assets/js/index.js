8fba0be1654ec51c981a2871008a0c581b11d91f
LANDOLSI Jasser <andolsijesser@gmail.com>
2022-10-12 10:07:51 +0200

    modif 2 jasser

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -10,21 +10,30 @@ vous trouverez leurs noms et descriptions dans les JSON téléchargés.
 /* Exemple de code d'utilisation de fetch :
 */
 fetch("../back/guitars.json")
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
+  .then(function (obj) {
+    if (obj.ok) {
+      return obj.json();
+    }
+    else {
+      throw new Error("le serveur ne répond pas");
     }
   })
-  .then(data => {
+  .then(function (objJS) {
+    for (let object of objJS) {
+      document.querySelector('#guitare').innerHTML += `<img src="../back/images/${object.imageUrl}"/>`; 
+      document.querySelector('#guitare').innerHTML += `${object.price}`; 
+      document.querySelector('#guitare').innerHTML += `${object.name}`; 
+      console.log(object.price)
+    }
     // Utilisation des données dé-jsonifiées dans l'objet data
-    console.table(data);
+    //console.table(data);
     // Regardez la structure du json, comprenez son contenu, intégrez-le
     // dans votre HTML.
   })
   .catch(function(error) {
     // Gestion des erreurs
-    console.error("Something goes wrong!");
-    console.error(error);
+    console.error(error.message);
+    
   });
 
   
